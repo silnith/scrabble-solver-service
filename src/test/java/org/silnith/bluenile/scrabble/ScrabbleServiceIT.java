8b2e13bfaa2f83606524d6341ef25816a31e8537
silnith <silnith@gmail.com>
2021-07-29 14:17:52 -0700

    Switch integration test to exercise the REST part.

--- src/test/java/org/silnith/bluenile/scrabble/ScrabbleServiceIT.java
+++ src/test/java/org/silnith/bluenile/scrabble/ScrabbleServiceIT.java
@@ -1,22 +1,32 @@
 package org.silnith.bluenile.scrabble;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ScrabbleServiceIT {
-
-  @Inject
-  private ScrabbleService service;
-
-  @Test
-  public void testApplicationStartsUp() {
-    Assert.assertNotNull(service);
-  }
+    
+    @Inject
+    private TestRestTemplate restTemplate;
+    
+    @Test
+    public void testApplicationStartsUp() {
+        final ResponseEntity<List> responseEntity = restTemplate.getForEntity("/words/{letters}", List.class, "foo");
+        
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        final List<?> body = responseEntity.getBody();
+        Assert.assertEquals(4, body.size());
+    }
+    
 }

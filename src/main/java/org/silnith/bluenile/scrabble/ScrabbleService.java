273d390d778ea7d3a5fc50aefeffc2c5029b9ed5
silnith <silnith@gmail.com>
2021-07-30 08:33:47 -0700

    Implement core sorting requirement.

--- src/main/java/org/silnith/bluenile/scrabble/ScrabbleService.java
+++ src/main/java/org/silnith/bluenile/scrabble/ScrabbleService.java
@@ -3,11 +3,21 @@ package org.silnith.bluenile.scrabble;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScrabbleService {
     
+    private final ScrabbleComparator comparator;
+    
+    @Inject
+    public ScrabbleService(final ScrabbleComparator comparator) {
+        super();
+        this.comparator = comparator;
+    }
+
     /**
      * Returns a list of words that can be spelled from the given set of letters.
      * It is sorted by its Scrabble point value.
@@ -16,7 +26,7 @@ public class ScrabbleService {
      * @return A sorted set of words
      */
     public SortedSet<String> getWords(final String letters) {
-        final SortedSet<String> words = new TreeSet<String>();
+        final SortedSet<String> words = new TreeSet<String>(comparator);
         
         words.add("dog");
         words.add("cat");
--- src/test/java/org/silnith/bluenile/scrabble/ScrabbleServiceTest.java
+++ src/test/java/org/silnith/bluenile/scrabble/ScrabbleServiceTest.java
@@ -3,17 +3,23 @@ package org.silnith.bluenile.scrabble;
 import java.util.SortedSet;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ScrabbleServiceTest {
+class ScrabbleServiceTest {
     
+    ScrabbleService scrabbleService;
+    
+    @BeforeEach
+    void setUp() throws Exception {
+        scrabbleService = new ScrabbleService(new ScrabbleComparator(new ScrabbleScorer()));
+    }
+
     @Test
     public void testGetWordsReturnsAListOfWords() {
-        ScrabbleService scrabbleService = new ScrabbleService();
-        
         SortedSet<String> words = scrabbleService.getWords("abc");
         Assertions.assertNotNull(words);
-        Assertions.assertEquals(words.size(), 4);
+        Assertions.assertEquals(4, words.size());
     }
     
 }

68acc192ba48c0b53203a20d3492aaa1001d02c8
silnith <silnith@gmail.com>
2021-08-01 10:31:15 -0700

    Return validation errors to the client.

--- src/main/java/org/silnith/bluenile/scrabble/JerseyConfig.java
+++ src/main/java/org/silnith/bluenile/scrabble/JerseyConfig.java
@@ -1,14 +1,16 @@
 package org.silnith.bluenile.scrabble;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class JerseyConfig extends ResourceConfig {
-
+    
     public JerseyConfig() {
         super(ScrabbleController.class);
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
     }
     
 }
